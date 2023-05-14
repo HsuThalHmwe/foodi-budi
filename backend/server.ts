9ffbeedf37571915bad7edaadc1e97f20f077bc9
HsuThalHmwe <hthucsmb@gmail.com>
2023-05-14 17:43:15 +0630

    sec commit

--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import cors from "cors";
 
 const app = express();
 const port = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//http:localhost5000/auth/register
+app.post("/auth/register", async (req: Request, res: Response) => {
+  res.send(req.body);
+});
 
 app.listen(port, () => {
   console.log("Server is running", port);
